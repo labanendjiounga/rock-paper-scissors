bfc67bc6721efcb617026b86c8db1a5229c7da50
Laban <317085441+laban-endji@users.noreply.github.com>
2026-09-03 07:44:42 +0100

    Write the logic to get the computer choice

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+// Write the logic to get the computer choice
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice = "";
+
+    switch (randomNumber) {
+        case 0:
+            computerChoice = "rock";
+            break;
+    
+        case 1:
+            computerChoice = "paper";
+            break;
+
+        case 2:
+            computerChoice = "scissors";
+            break;
+    }
+    
+    return computerChoice;
+}
